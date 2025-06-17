60cb959fdba5f221846d6fd62e099b52da840b62
Marvin Zeising <marvin.zeising@staffbase.com>
2025-06-17 13:48:29 +0200

    remove useless id

--- backend/posts/NewsPost.js
+++ backend/posts/NewsPost.js
@@ -8,7 +8,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const NewsPostSchema = new mongoose.Schema({
-  id: String,
   title: String,
   description: String,
   content: String,
